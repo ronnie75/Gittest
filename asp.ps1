1f846779b8785cfef48d0a539c8fd690f9ccde6c
Di Micco <Salvatore.DiMicco@cirquedusoleil.com>
2020-02-21 13:04:50 -0500

    delte

--- asp.ps1
+++
@@ -1,5 +0,0 @@
-﻿$cred = Get-azureAutomationCredential -Name 'AzureCredentialsKT' -AutomationAccountName 'testAutomation'
-$SubscriptionId = Get-AzureAutomationVariable -Name 'AzureSubscriptionId'
-$account = Add-AzureRMAccount -credential $cred
-Select-AzureRmSubscription -SubscriptionId $SubscriptionId
-Set-AzureRmAppServicePlan -Name "MyAppServicePlan" -ResourceGroupName "MyRG" -Tier Standard -WorkerSize Medium
