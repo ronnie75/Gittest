50a1f22541873c181d08a8f20cf4eac3ad6452a1
ronnie75 <ohoudart@yahoo.fr>
2020-02-21 10:42:01 -0500

    Add files via upload

---
+++ asp.ps1
@@ -0,0 +1,5 @@
+﻿$cred = Get-azureAutomationCredential -Name 'AzureCredentialsKT' -AutomationAccountName 'testAutomation'
+$SubscriptionId = Get-AzureAutomationVariable -Name 'AzureSubscriptionId'
+$account = Add-AzureRMAccount -credential $cred
+Select-AzureRmSubscription -SubscriptionId $SubscriptionId
+Set-AzureRmAppServicePlan -Name "MyAppServicePlan" -ResourceGroupName "MyRG" -Tier Standard -WorkerSize Medium
